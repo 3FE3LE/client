f4540a039708fb4ff158189d6c359d4cab8bffcc
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-06-08 19:00:40 -0500

    feat: :sparkles: add admin suite app to monorepo

--- apps/suite/app/page.tsx
+++ apps/suite/app/page.tsx
@@ -7,7 +7,7 @@ import styles from "../styles/index.module.css";
 export default function Web() {
   return (
     <div className={styles.container}>
-      <h1>Este el admin dashboard</h1>
+      <h1>Esta es la suite</h1>
       <Button onClick={() => console.log("Pressed!")} text="Boop" />
     </div>
   );

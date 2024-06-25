d2826104da9936b3f6722384e723b2bd27abd0ea
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-06-24 19:28:21 -0500

    feat(suite): :sparkles: add authenticacion on Dashboard page

--- apps/suite/app/dashboard/page.tsx
+++ apps/suite/app/dashboard/page.tsx
@@ -1,8 +1,28 @@
-export default function Dashboard() {
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
+import { Session } from 'next-auth';
+import SignOutButton from './components/SignOutButton';
+
+// Esta función se ejecutará en el servidor
+const getSessionOnServer = async (): Promise<Session | null> => {
+  return await getServerSession(authOptions);
+};
+
+const DashboardPage = async () => {
+  const session = await getSessionOnServer();
+  console.log('session:', session);
+  if (!session) {
+    redirect('/login');
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
-      <p>Welcome to the dashboard!</p>
+      <p>Welcome, {session?.user?.email}</p>
+      <SignOutButton />
     </div>
   );
-}
+};
+
+export default DashboardPage;

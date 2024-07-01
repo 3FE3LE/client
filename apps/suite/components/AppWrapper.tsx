5ce02de18da491e27c3d5207badd6e796aa20bad
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-07-01 17:49:28 -0500

    feat(global): :heavy_plus_sign: add & configure react-hot-toast

--- apps/suite/components/AppWrapper.tsx
+++ apps/suite/components/AppWrapper.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
 
 export const AppWrapper = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <Toaster />
+      {children}
+    </SessionProvider>
+  );
 };

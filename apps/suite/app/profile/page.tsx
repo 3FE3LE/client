0503bfdc5b9a5e86ddf8a51f7cdfb61df064f31b
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-07-01 18:42:10 -0500

    feat(suite): :sparkles: add profile page

---
+++ apps/suite/app/profile/page.tsx
@@ -0,0 +1,18 @@
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import { Navbar } from '../../components';
+import { Session } from 'next-auth';
+
+const getSessionOnServer = async (): Promise<Session | null> => {
+  return await getServerSession(authOptions);
+};
+
+export default async function ProfilePage() {
+  const session = await getSessionOnServer();
+  if (!session) {
+    redirect('/login');
+  }
+
+  return <div>You need to be authenticated to view this page.</div>;
+}

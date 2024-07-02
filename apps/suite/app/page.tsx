afc6eed24080a22c1976abf1ce2cfb14879e6117
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-07-02 11:09:09 -0500

    feat(suite): :construction: WIP landing page add image

--- apps/suite/app/page.tsx
+++ apps/suite/app/page.tsx
@@ -1,16 +1,28 @@
+import Image from 'next/image';
 import { Navbar } from '../components';
+import traveling_img from '@repo/ui/assets/traveling.svg';
 
 export default function Web() {
   return (
-    <div>
+    <>
       <Navbar />
-      <h1 className="heading--1">
-        Simplify your travels, maximize your experiences
-      </h1>
-      <h5 className="heading--2">
-        Destination, budget, activities, and more - all integrated into a single
-        app
-      </h5>
-    </div>
+      <main className="home__container">
+        <div className="home__title">
+          <h1 className="heading--1">
+            Simplify your travels, maximize your experiences
+          </h1>
+          <h5 className="subtitle--1">
+            Destination, budget, activities, and more - all integrated into a
+            single app
+          </h5>
+        </div>
+        <Image
+          src={traveling_img}
+          alt="traveling image"
+          width={550}
+          height={550}
+        />
+      </main>
+    </>
   );
 }

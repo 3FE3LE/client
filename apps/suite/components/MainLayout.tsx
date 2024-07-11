d92b8ae2d0ac66c3ac785c9b53765bd44dbd2cf0
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-07-11 12:51:09 -0500

    feat(suite): :sparkles: add MainLayout, Sidebar & ThemeSwitcher components

---
+++ apps/suite/components/MainLayout.tsx
@@ -0,0 +1,16 @@
+import { PageProps } from '@/app/types';
+
+import { Navbar } from './Navbar';
+import { Sidebar } from './Sidebar';
+
+export const MainLayout = ({ children, params: { locale } }: PageProps) => {
+  return (
+    <main className="main">
+      <Navbar locale={locale} />
+      <div className="main__content">
+        <Sidebar />
+        {children}
+      </div>
+    </main>
+  );
+};
---
+++ apps/suite/components/ThemeSwitcher.tsx
@@ -0,0 +1,27 @@
+'use client';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+
+export const ThemeSwitcher = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const localTheme = window.localStorage.getItem('theme');
+  setTheme(localTheme as string);
+  return (
+    <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+      <option value="system">System</option>
+      <option value="dark">Dark</option>
+      <option value="light">Light</option>
+    </select>
+  );
+};

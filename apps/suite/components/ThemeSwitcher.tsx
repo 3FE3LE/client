8156e2d2880653023a74492482c1d93091de262c
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-07-21 11:59:48 -0500

    feat(suite): :sparkles: New look & feel on ThemeSwitcher component

--- apps/suite/components/ThemeSwitcher.tsx
+++ apps/suite/components/ThemeSwitcher.tsx
@@ -2,10 +2,19 @@
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 
+import { Monitor, Moon, Sun } from '@repo/ui';
+
 export const ThemeSwitcher = () => {
+  const themes = [
+    { name: 'system', icon: Monitor },
+    { name: 'dark', icon: Moon },
+    { name: 'light', icon: Sun },
+  ];
+
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
-
+  const [showOptions, setShowOptions] = useState(false);
+  const selectedTheme = themes.find((th) => th.name === theme);
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
     setMounted(true);
@@ -16,10 +25,30 @@ export const ThemeSwitcher = () => {
   }
 
   return (
-    <select value={theme} onChange={(e) => setTheme(e.target.value)}>
-      <option value="system">System</option>
-      <option value="dark">Dark</option>
-      <option value="light">Light</option>
-    </select>
+    <div className="th-sw" onClick={() => setShowOptions(!showOptions)}>
+      <span className="">Theme</span>
+      <button className={`th-sw__btn `}>
+        {selectedTheme && <selectedTheme.icon />}
+      </button>
+      {showOptions && (
+        <div className="th-sw__options">
+          {themes.map(
+            (option) =>
+              theme !== option.name && (
+                <button
+                  key={option.name}
+                  className={`th-sw__btn`}
+                  onClick={() => {
+                    setTheme(option.name);
+                    setShowOptions(!showOptions);
+                  }}
+                >
+                  <option.icon />
+                </button>
+              ),
+          )}
+        </div>
+      )}
+    </div>
   );
 };

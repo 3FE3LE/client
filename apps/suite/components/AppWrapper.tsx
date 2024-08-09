970e7c5ad38192faf8e7aa1ce49eb2c8da13feab
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-08-09 17:03:15 -0500

    refactor(global): :zap: descentralized appwrapper component & generate on each app, relocate & update SignOutButton

---
+++ apps/suite/components/AppWrapper.tsx
@@ -0,0 +1,35 @@
+import { SessionProvider } from 'next-auth/react';
+import { NextIntlClientProvider } from 'next-intl';
+import { ThemeProvider } from 'next-themes';
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+
+export type AppWrapperProps = {
+  children: React.ReactNode;
+  messages: any;
+  locale: string;
+};
+
+const timeZone = 'America/Bogota';
+
+export const AppWrapper = ({ children, messages, locale }: AppWrapperProps) => {
+  return (
+    <SessionProvider>
+      <NextIntlClientProvider
+        messages={messages}
+        locale={locale}
+        timeZone={timeZone}
+      >
+        <ThemeProvider
+          enableColorScheme={true}
+          defaultTheme="system"
+          storageKey="theme"
+          themes={['system', 'dark', 'light']}
+        >
+          {children}
+          <Toaster position="bottom-center" />
+        </ThemeProvider>
+      </NextIntlClientProvider>
+    </SessionProvider>
+  );
+};

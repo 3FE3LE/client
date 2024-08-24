acde3e4b641e697ff1a7403f50f00b382673ade2
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-08-24 13:21:06 -0500

    chore(global): :fire: delete icon components & add InputField component from @repo/ui package

---
+++ packages/ui/src/components/InputField.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export type InputFieldProps = {
+  name: string;
+  placeholder?: string;
+  value?: string;
+  handleChange: (value: string) => void;
+  handleSubmit?: () => void;
+  children?: React.ReactNode;
+};
+export const InputField: React.FC<InputFieldProps> = ({
+  name,
+  children,
+  placeholder,
+  value,
+  handleChange,
+  handleSubmit,
+}) => {
+  const handleEnterKey = (key: any) => {
+    if (key === 'Enter' && handleSubmit) {
+      handleSubmit();
+    }
+  };
+
+  return (
+    <fieldset className="form__group">
+      <label className="form__label" htmlFor="name">
+        {name}
+      </label>
+      <input
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        type="text"
+        id={name}
+        onChange={(e) => handleChange(e.target.value)}
+        onKeyDown={(e) => handleEnterKey(e.key)}
+      />
+      {children}
+    </fieldset>
+  );
+};
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -10,21 +10,10 @@ export {
   type EyeIconToggleProps,
 } from './components/EyeIconToggle';
 export { FormWrapper, type FormWrapperProps } from './components/FormWrapper';
+export { InputField, type InputFieldProps } from './components/InputField';
 export { InputGroup, type InputProps } from './components/InputGroup';
 export {
   SubmitButton,
   type SubmitButtonProps,
 } from './components/SubmitButton';
 export { ThemeSwitcher } from './components/ThemeSwitcher';
-// ICONS
-export { Eye } from './icons/Eye';
-export { EyeOff } from './icons/EyeOff';
-export { Google } from './icons/Google';
-export { LeftArrow } from './icons/LeftArrow';
-export { LeftCircleArrow } from './icons/LeftCircleArrow';
-export { Monitor } from './icons/Monitor';
-export { Moon } from './icons/Moon';
-export { NotFoundImage } from './icons/NotFoundImage';
-export { Plane } from './icons/Plane';
-export { Sun } from './icons/Sun';
-export { UserCircle } from './icons/UserCircle';
